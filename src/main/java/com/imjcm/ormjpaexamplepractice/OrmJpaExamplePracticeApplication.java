d55fc57dfccdc36c3617f8278f7dee4513c86a0b
ImJcm <wjsckdals0909@naver.com>
2024-05-21 01:19:07 +0900

    ORM-JPA init setting

---
+++ src/main/java/com/imjcm/ormjpaexamplepractice/OrmJpaExamplePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.imjcm.ormjpaexamplepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmJpaExamplePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrmJpaExamplePracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/imjcm/ormjpaexamplepractice/OrmJpaExamplePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imjcm.ormjpaexamplepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmJpaExamplePracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
