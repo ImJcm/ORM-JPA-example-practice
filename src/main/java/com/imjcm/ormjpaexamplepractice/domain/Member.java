bb8531370cc48d6d611d347035dcd726c294301b
ImJcm <wjsckdals0909@naver.com>
2024-06-19 02:20:34 +0900

    Member Entity update

--- src/main/java/com/imjcm/ormjpaexamplepractice/domain/Member.java
+++ src/main/java/com/imjcm/ormjpaexamplepractice/domain/Member.java
@@ -1,8 +1,14 @@
 package com.imjcm.ormjpaexamplepractice.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 @Table(name = "member")
 public class Member {
     @Id
@@ -15,4 +21,16 @@ public class Member {
     @Column(name = "age", nullable = false)
     private int age;
 
+    @Builder
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
+    public static Member createMember(String username, int age) {
+        return Member.builder()
+                .username(username)
+                .age(age)
+                .build();
+    }
 }
