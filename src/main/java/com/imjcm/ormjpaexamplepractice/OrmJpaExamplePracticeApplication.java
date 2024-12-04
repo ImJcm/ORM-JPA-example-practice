e35d2e3641c9924546554e2aa15baf9829fd579f
ImJcm <wjsckdals0909@naver.com>
2024-12-05 02:43:33 +0900

    Enable JPA Auditing Annotation 설정

--- src/main/java/com/imjcm/ormjpaexamplepractice/OrmJpaExamplePracticeApplication.java
+++ src/main/java/com/imjcm/ormjpaexamplepractice/OrmJpaExamplePracticeApplication.java
@@ -1,13 +1,17 @@
 package com.imjcm.ormjpaexamplepractice;
 
+import com.imjcm.ormjpaexamplepractice.domain.Member;
+import com.imjcm.ormjpaexamplepractice.global.Role;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // timestamp 시간 자동 생성 적용
 public class OrmJpaExamplePracticeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(OrmJpaExamplePracticeApplication.class, args);
     }
-
 }
