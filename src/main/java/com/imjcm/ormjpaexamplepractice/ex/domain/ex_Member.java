b88d98301ab503c9b58febe07c55cf80f1701b17
ImJcm <wjsckdals0909@naver.com>
2025-02-14 23:45:14 +0900

    orm jpa - Example - Member Entity setting

--- src/main/java/com/imjcm/ormjpaexamplepractice/ex/domain/ex_Member.java
+++ src/main/java/com/imjcm/ormjpaexamplepractice/ex/domain/ex_Member.java
@@ -1,4 +1,34 @@
 package com.imjcm.ormjpaexamplepractice.ex.domain;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "ex_Member")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class ex_Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+    private String address;
+
+    @Builder
+    public ex_Member(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    // @Setter
+    public void setName(String name) { this.name = name; }
+
+    // @Setter
+    public void setAddress(String address) { this.address = address; }
+
 }
