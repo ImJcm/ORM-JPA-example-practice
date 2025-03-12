7b5d739c87b7ec2a6e0a0b08028faeffb0b2b02a
ImJcm <wjsckdals0909@naver.com>
2025-03-12 12:34:31 +0900

    package + food,member -> member,cart test code update

--- src/test/java/com/imjcm/ormjpaexamplepractice/지연_로딩_테스트_클래스.java
+++ src/test/java/com/imjcm/ormjpaexamplepractice/지연_로딩_테스트_클래스.java
@@ -1,10 +1,13 @@
 package com.imjcm.ormjpaexamplepractice;
 
-import com.imjcm.ormjpaexamplepractice.domain.Food;
-import com.imjcm.ormjpaexamplepractice.domain.Member;
-import com.imjcm.ormjpaexamplepractice.global.Role;
-import com.imjcm.ormjpaexamplepractice.repository.FoodRepository;
-import com.imjcm.ormjpaexamplepractice.repository.MemberRepository;
+import com.imjcm.ormjpaexamplepractice.prac.domain.Cart;
+import com.imjcm.ormjpaexamplepractice.prac.domain.CartFood;
+import com.imjcm.ormjpaexamplepractice.prac.domain.Food;
+import com.imjcm.ormjpaexamplepractice.prac.domain.Member;
+import com.imjcm.ormjpaexamplepractice.prac.global.Role;
+import com.imjcm.ormjpaexamplepractice.prac.repository.CartRepository;
+import com.imjcm.ormjpaexamplepractice.prac.repository.FoodRepository;
+import com.imjcm.ormjpaexamplepractice.prac.repository.MemberRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import org.assertj.core.api.Assertions;
@@ -14,10 +17,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Calendar;
+import java.util.Date;
+
 @SpringBootTest
 public class 지연_로딩_테스트_클래스 {
     @Autowired
     private MemberRepository memberRepository;
+
+    @Autowired
+    private CartRepository cartRepository;
 
     @Autowired
     private FoodRepository foodRepository;
@@ -37,9 +46,12 @@ public class 지연_로딩_테스트_클래스 {
         Member member = Member.builder()
                 .username("JCM")
                 .age(29)
+                .birthday(new Date(1997 - 1900, Calendar.SEPTEMBER,9))
                 .description("jcm")
                 .role(Role.USER)
                 .build();
+
+        Cart cart = new Cart();
 
         Food food1 = Food.builder()
                 .name("food1")
@@ -51,8 +63,17 @@ public class 지연_로딩_테스트_클래스 {
                 .price(2000L)
                 .build();
 
-        member.addFoodList(food1);
-        member.addFoodList(food2);
+        CartFood cartFood1 = CartFood.builder()
+                .cart(cart)
+                .food(food1)
+                .build();
+
+        CartFood cartFood2 = CartFood.builder()
+                .cart(cart)
+                .food(food2)
+                .build();
+
+        member.applyCart(cart);
 
         memberRepository.save(member);
 
@@ -67,7 +88,7 @@ public class 지연_로딩_테스트_클래스 {
     public void 연관관계_객체가_프록시객체라면_성공() {
         Member member = memberRepository.findById(1L).orElseThrow(RuntimeException::new);
 
-        boolean isLoad = entityManager.getEntityManagerFactory().getPersistenceUnitUtil().isLoaded(member.getFoodList());
+        boolean isLoad = entityManager.getEntityManagerFactory().getPersistenceUnitUtil().isLoaded(member.getCart());
 
         Assertions.assertThat(isLoad).isEqualTo(false);
     }
@@ -78,9 +99,9 @@ public class 지연_로딩_테스트_클래스 {
     public void 연관관계_객체가_초기화_되었다면_성공() {
         Member member = memberRepository.findById(1L).orElseThrow(RuntimeException::new);
 
-        int size = member.getFoodList().size();
+        Member exist_member = member.getCart().getMember();
 
-        boolean isLoad = entityManager.getEntityManagerFactory().getPersistenceUnitUtil().isLoaded(member.getFoodList());
+        boolean isLoad = entityManager.getEntityManagerFactory().getPersistenceUnitUtil().isLoaded(member.getCart());
 
         Assertions.assertThat(isLoad).isEqualTo(true);
     }
