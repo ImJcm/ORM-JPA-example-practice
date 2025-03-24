d31627033d3467f09a467e9d6bf87fbdad15d492
ImJcm <wjsckdals0909@naver.com>
2025-03-25 04:08:51 +0900

    JoinColumn 속성 예시

--- src/main/java/com/imjcm/ormjpaexamplepractice/prac/domain/CartFood.java
+++ src/main/java/com/imjcm/ormjpaexamplepractice/prac/domain/CartFood.java
@@ -20,7 +20,16 @@ public class CartFood {
     private Cart cart;
 
     @ManyToOne
-    @JoinColumn(name = "food_id")
+    @JoinColumn(
+            name = "food_id",
+            referencedColumnName = "FOOD_ID",
+            unique = false,
+            nullable = true,
+            insertable = true,
+            updatable = true,
+            columnDefinition = "varchar(30) default 'EMPTY'",
+            table = "food"
+    )
     private Food food;
 
     @Builder
