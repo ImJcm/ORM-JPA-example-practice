480d28a86d1675539adeeef2a1fbe36145fffe48
ImJcm <wjsckdals0909@naver.com>
2025-04-21 03:06:23 +0900

    Cart Entity - Member 외래 키 주인 관리 설정

--- src/main/java/com/imjcm/ormjpaexamplepractice/prac/domain/Cart.java
+++ src/main/java/com/imjcm/ormjpaexamplepractice/prac/domain/Cart.java
@@ -3,16 +3,38 @@ package com.imjcm.ormjpaexamplepractice.prac.domain;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Getter
 @Table(name = "cart")
 public class Cart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    //@Column(name = "CART_ID")
     private long id;
 
-    @Setter
-    @OneToOne(mappedBy = "cart") // 연관관계 주인을 설정 - Member.cart
+    // 연관관계의 주인 - 외래 키 관리
+    @OneToOne
+    @JoinColumn(name = "member_id") // Member Entity's Id - Table Name_id(default) = Column Name
     private Member member;
+
+    @Setter
+    @OneToMany(mappedBy = "cart")
+    private List<CartFood> cartFoods = new ArrayList<>();
+
+    // 연관관계 매핑 편의 메서드
+    public void setMember(Member member) {
+        if(this.member != null) {
+            this.member.setCart(null);
+        }
+
+        this.member = member;
+        this.member.setCart(this);
+    }
+
+    public void addCartFood(CartFood cartfood) {
+        this.cartFoods.add(cartfood);
+        cartfood.setCart(this);
+    }
 }
