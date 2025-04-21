78ac2add896b882cb7bb981cb6e7bb071f084653
ImJcm <wjsckdals0909@naver.com>
2025-04-22 05:57:12 +0900

    CartItem - food -> item 변경

--- src/main/java/com/imjcm/ormjpaexamplepractice/prac/repository/CartItemRepository.java
+++ src/main/java/com/imjcm/ormjpaexamplepractice/prac/repository/CartItemRepository.java
@@ -1,9 +1,9 @@
 package com.imjcm.ormjpaexamplepractice.prac.repository;
 
-import com.imjcm.ormjpaexamplepractice.prac.domain.CartFood;
+import com.imjcm.ormjpaexamplepractice.prac.domain.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CartFoodRepository extends JpaRepository<CartFood, Long> {
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 }
