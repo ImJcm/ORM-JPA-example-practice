066e4b6763bce9963abe288a64dc949a408dd2de
ImJcm <wjsckdals0909@naver.com>
2025-04-22 05:57:25 +0900

    Item - food -> item 변경

--- src/main/java/com/imjcm/ormjpaexamplepractice/prac/repository/ItemRepository.java
+++ src/main/java/com/imjcm/ormjpaexamplepractice/prac/repository/ItemRepository.java
@@ -1,9 +1,9 @@
 package com.imjcm.ormjpaexamplepractice.prac.repository;
 
-import com.imjcm.ormjpaexamplepractice.prac.domain.Food;
+import com.imjcm.ormjpaexamplepractice.prac.domain.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FoodRepository extends JpaRepository<Food, Long> {
+public interface ItemRepository extends JpaRepository<Item, Long> {
 }
