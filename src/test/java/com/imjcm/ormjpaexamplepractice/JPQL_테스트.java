b746422879199e0f825f7df02c618ec7e1f4f2d3
ImJcm <wjsckdals0909@naver.com>
2025-04-22 05:58:15 +0900

    JPQL Test - Order/OrderItem 엔티티 조회 테스트 추가

--- src/test/java/com/imjcm/ormjpaexamplepractice/JPQL_테스트.java
+++ src/test/java/com/imjcm/ormjpaexamplepractice/JPQL_테스트.java
@@ -1,15 +1,9 @@
 package com.imjcm.ormjpaexamplepractice;
 
 
-import com.imjcm.ormjpaexamplepractice.prac.domain.Cart;
-import com.imjcm.ormjpaexamplepractice.prac.domain.CartFood;
-import com.imjcm.ormjpaexamplepractice.prac.domain.Food;
-import com.imjcm.ormjpaexamplepractice.prac.domain.Member;
+import com.imjcm.ormjpaexamplepractice.prac.domain.*;
 import com.imjcm.ormjpaexamplepractice.prac.global.Role;
-import com.imjcm.ormjpaexamplepractice.prac.repository.CartFoodRepository;
-import com.imjcm.ormjpaexamplepractice.prac.repository.CartRepository;
-import com.imjcm.ormjpaexamplepractice.prac.repository.FoodRepository;
-import com.imjcm.ormjpaexamplepractice.prac.repository.MemberRepository;
+import com.imjcm.ormjpaexamplepractice.prac.repository.*;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -17,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -35,14 +30,24 @@ public class JPQL_테스트 {
     private CartRepository cartRepository;
 
     @Autowired
-    private CartFoodRepository cartFoodRepository;
+    private CartItemRepository cartItemRepository;
 
     @Autowired
-    private FoodRepository foodRepository;
+    private ItemRepository itemRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
     void clear_db() {
         memberRepository.deleteAll();
         cartRepository.deleteAll();
+        cartItemRepository.deleteAll();
+        itemRepository.deleteAll();
+        orderRepository.deleteAll();
+        orderItemRepository.deleteAll();
     }
 
     @BeforeEach
@@ -55,6 +60,7 @@ public class JPQL_테스트 {
                 .birthday(new Date(1997 - 1900, Calendar.SEPTEMBER,9))
                 .description("jcm")
                 .role(Role.USER)
+                .orders(new ArrayList<>())
                 .build();
 
         Member member2 = Member.builder()
@@ -63,23 +69,24 @@ public class JPQL_테스트 {
                 .birthday(new Date(1997 - 1900, Calendar.SEPTEMBER,9))
                 .description("cmj")
                 .role(Role.USER)
+                .orders(new ArrayList<>())
                 .build();
 
         memberRepository.save(member);
         memberRepository.save(member2);
 
-        Food food1 = Food.builder()
-                .name("food1")
+        Item item1 = Item.builder()
+                .name("item1")
                 .price(1000L)
                 .build();
 
-        Food food2 = Food.builder()
-                .name("food2")
+        Item item2 = Item.builder()
+                .name("item2")
                 .price(2000L)
                 .build();
 
-        foodRepository.save(food1);
-        foodRepository.save(food2);
+        itemRepository.save(item1);
+        itemRepository.save(item2);
 
         Cart cart = new Cart();
         cart.setMember(member);
@@ -90,28 +97,48 @@ public class JPQL_테스트 {
         cartRepository.save(cart);
         cartRepository.save(cart2);
 
-        CartFood cartFood1 = CartFood.builder()
+        CartItem cartItem1 = CartItem.builder()
                 .cart(cart)
-                .food(food1)
+                .item(item1)
                 .build();
 
-        CartFood cartFood2 = CartFood.builder()
+        CartItem cartItem2 = CartItem.builder()
                 .cart(cart)
-                .food(food2)
+                .item(item2)
                 .build();
 
-        CartFood cartFood3 = CartFood.builder()
+        CartItem cartItem3 = CartItem.builder()
                 .cart(cart2)
-                .food(food2)
+                .item(item2)
                 .build();
 
-        cartFoodRepository.save(cartFood1);
-        cartFoodRepository.save(cartFood2);
-        cartFoodRepository.save(cartFood3);
+        cartItemRepository.save(cartItem1);
+        cartItemRepository.save(cartItem2);
+        cartItemRepository.save(cartItem3);
 
-        cart.addCartFood(cartFood1);
-        cart.addCartFood(cartFood2);
-        cart2.addCartFood(cartFood3);
+        cart.addCartItem(cartItem1);
+        cart.addCartItem(cartItem2);
+        cart2.addCartItem(cartItem3);
+
+        Order order = Order.builder()
+                .orderItems(new ArrayList<>())
+                .orderDate(new Date())
+                .build();
+
+        order.setMember(member);
+
+        orderRepository.save(order);
+
+        OrderItem orderItem = OrderItem.builder()
+                .order(order)
+                .orderPrice(item1.getPrice())
+                .item(item1)
+                .count(1)
+                .build();
+
+        orderItemRepository.save(orderItem);
+
+        order.addOrderItem(orderItem);
 
         /*entityManager.flush();
         entityManager.clear();
@@ -121,12 +148,26 @@ public class JPQL_테스트 {
     @Test
     @DisplayName("EntityManager.createQuery() - 엔티티_조회_used_by_JPQL")
     public void 엔티티_조회_used_by_JPQL() {
-        String jpql = "select cf from CartFood cf join cart c where c.member.username =: memberName";
+        String jpql = "select ci from CartItem cf join cart c where c.member.username =: memberName";
 
-        List<CartFood> resultList = entityManager.createQuery(jpql, CartFood.class)
+        List<CartItem> resultList = entityManager.createQuery(jpql, CartItem.class)
                 .setParameter("memberName", "JCM")
                 .getResultList();
 
         assertThat(resultList.size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("EntityManager.createQuery() - 엔티티_조회2_user_by_JPQL")
+    public void 엔티티_조회_user_by_JPQL2() {
+        String jpql = "select oi from OrderItem oi join order o where o.member.username =: memberName";
+        // jpql - 데이터베이스 테이블 기준 x -> 객체 엔티티명 기준이므로 orders가 아닌 order이다.
+        // 즉, 객체 엔티티를 기준으로 봐야한다.
+
+        List<OrderItem> resultList = entityManager.createQuery(jpql, OrderItem.class)
+                .setParameter("memberName", "JCM")
+                .getResultList();
+
+        assertThat(resultList.size()).isEqualTo(1);
     }
 }
