f4b48f627603c8b7f09ed86291cac3d7c1b743a8
ImJcm <wjsckdals0909@naver.com>
2025-04-22 05:59:23 +0900

    OrderRepository 추가

---
+++ src/main/java/com/imjcm/ormjpaexamplepractice/prac/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.imjcm.ormjpaexamplepractice.prac.repository;
+
+import com.imjcm.ormjpaexamplepractice.prac.domain.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
